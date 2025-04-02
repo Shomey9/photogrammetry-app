a671ccd5e97c37cb28f931e43dbd010b42a836ef
shomey9 <adyrendusara@gmail.com>
2025-04-02 00:12:07 -0500

    Add cesiumJs application initializer with reactivity but is not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { CesiumService } from './services/cesium/cesium.service';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'photogrammetry-app';
+export class AppComponent implements OnInit {
+  title = 'cesium-angular-app';
+  cesiumService = inject(CesiumService);
+
+  ngOnInit() {
+    tap(() => console.log('Initializing Cesium...')),
+    // Initialize Cesium and subscribe to the observable to get updates
+    this.cesiumService
+      .initializeCesium('cesiumContainer').subscribe()
+
+      //initializer is not working properly
+  }
 }
